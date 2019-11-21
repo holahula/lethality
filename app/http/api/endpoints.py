88529db31a718b1c41bcbd6aaa64390300ca7229
Mogen Cheng <mogen@Mogens-MacBook-Pro.local>
2019-11-21 14:02:31 -0500

    fixed endpoint foking me up

--- app/http/api/endpoints.py
+++ app/http/api/endpoints.py
@@ -1,4 +1,4 @@
-#FLASK_APP=$PWD/app/http/api/endpoints.py FLASK_ENV=development python -m flask run --port=4433
+#FLASK_APP=$PWD/app/http/api/endpoints.py FLASK_ENV=development python3 -m flask run --port=4433
 
 from flask import Flask, json, g, request
 from app.idp.service import Service as IdentityService
