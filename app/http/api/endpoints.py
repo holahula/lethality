7d2619822e8e49902d7b72c8971a32d426b07082
Austin Jiang <austin.jiang@uwaterloo.ca>
2019-11-22 02:24:26 -0500

    remove un-needed comments

--- app/http/api/endpoints.py
+++ app/http/api/endpoints.py
@@ -40,8 +40,6 @@ def fill_puzzle_data(puzzle):
         except AttributeError as error:
             print("ERROR:", error, area_name)
 
-# https://developer.okta.com/blog/2018/12/20/crud-app-with-python-flask-react
-
 @app.route("/", methods = ["GET"])
 def entry():
     return json_response({"yes": "no"}, 200)
